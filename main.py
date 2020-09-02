a4a6a81eb8de087bc6ec496418e733f5112512dc
Paulo Tintaya <ptintaya70@gmail.com>
2020-09-02 13:37:33 -0400

    prueba manos frame

--- main.py
+++ main.py
@@ -4,7 +4,62 @@ from tkinter import font
 
 def captarHuellaWindow():
     huella_window = tk.Tk()
-    tk.Label(window, text='Hola Mundo :3')
+    huella_window.configure(background="#ebac00")
+    huella_window.geometry("1200x600")
+
+
+    tk.Label(huella_window, text="SISTEMA DE RECONOCIMENTO DE PATRONES DACTILARES", bg='#ebac00',
+             font=("Arial", "24", "bold italic")).place(x=233,y=30)
+    tk.Label(huella_window, text="BASADO EN LA TEORIA DE DERMATOGLIFIOS, PARA DETERMINAR", bg='#ebac00',
+             font=("Arial", "24", "bold italic")).place(x=196, y=60)
+    tk.Label(huella_window, text="LOS TALENTOS Y CAPACIDADES GENÉTICAS DE LOS DEPORTISTAS", bg='#ebac00',
+             font=("Arial", "24", "bold italic")).place(x=189, y=90)
+
+
+
+    tk.LabelFrame(huella_window, bg='black', height=350, width=1150).place(relx=0.5, rely=0.5, anchor=tk.CENTER,y=35)
+
+    tk.LabelFrame(huella_window, bg='#EB5E00', height=319, width=325).place(x=36,y=175)
+
+    # MANOSFRAME(Problemas)
+    #   imageHuella = tk.PhotoImage(file="image/manos-4.gif")
+    #   l = tk.Label(huella_window, image=imageHuella).place(x=36, y=36)
+
+
+    '''
+    #BTNGuardar
+    btn_guardar = tk.Button(
+        huella_window,
+        highlightbackground='yellow',
+        text='GUARDAR',
+        font=('Arial Rounded MT Bold', 14),
+        width=20
+        #   command = guardar
+    ).place(x=200, y=900)
+
+    # btnResultados
+    btn_resultados = tk.Button(
+        huella_window,
+        highlightbackground='yellow',
+        text='Resultados',
+        font=('Arial Rounded MT Bold', 14),
+        width=20
+        #   command = resultados
+    ).place(x=250, y=1000)
+
+    # Btn Volver
+    btn_volver = tk.Button(
+        huella_window,
+        highlightbackground='yellow',
+        text='Volver',
+        font=('Arial Rounded MT Bold', 14),
+        width=20
+        # command = huella_window.destroy()
+    ).place(x=300, y=1100)
+    
+    '''
+
+
     huella_window.resizable(width=False, height=False)
     huella_window.mainloop()
 
@@ -41,6 +96,7 @@ labelEmi= tk.Label(window, image= imageEmi).place(x=855,y=100)
 #CARTA DERMATOGLIFIA
 btn_resultados = tk.Button(
     window,
+    highlightbackground='#FBFF00',
     text = 'CARTA DERMATOGLÍFIA',
     font = ('Arial Rounded MT Bold', 14),
     width = 20
@@ -50,6 +106,7 @@ btn_resultados = tk.Button(
 #CONFIGURACION
 btn_configuracion = tk.Button(
     window,
+    highlightbackground='#04FEEF',
     text = 'CONFIGURACIÓN',
     font = ('Arial Rounded MT Bold', 14),
     width = 20
@@ -59,6 +116,7 @@ btn_configuracion = tk.Button(
 #CAPTAR HUELLA
 btn_huella = tk.Button(
     window,
+    highlightbackground='#08FE04',
     text = 'CAPTAR HUELLA',
     font = ('Arial Rounded MT Bold', 14),
     width = 20,
@@ -69,6 +127,7 @@ btn_huella = tk.Button(
 #Acerca del autor
 btn_author = tk.Button(
     window,
+    highlightbackground='#FFC638',
     text = 'ACERCA DEL AUTOR',
     font = ('Arial Rounded MT Bold', 14),
     width = 20
@@ -78,6 +137,7 @@ btn_author = tk.Button(
 #Volver
 btn_volver = tk.Button(
     window,
+    highlightbackground='#FFC638',
     text = 'VOLVER',
     font = ('Arial Rounded MT Bold', 14),
     width = 20
